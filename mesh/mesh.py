275087e9d298d84b320e6b44b98a440602d694ad
Robert Gardner <rhg259@nyu.edu>
2014-10-30 01:22:41 -0400

    Update mesh and tweet a watt

--- mesh/mesh.py
+++ mesh/mesh.py
@@ -1,22 +1,33 @@
+#!/usr/bin/env python
 """
 Continuously read the serial port and process IO data received from a remote
 XBee.
 """
 
-from xbee import XBee
 import serial
+import time
+from xbee import ZigBee  # Xbee Series 2
 
-SERIALPORT = "/dev/tty.usbserial-AH02QMT3"
+SERIALPORT = "/dev/tty.usbserial-A602N3AY"
+# SERIALPORT = "/dev/tty.usbserial-AH02QMT3"
+
+
+def print_data(data):
+    """
+    This method is called whenever data is received from the associated XBee
+    device. Its first and only argument is the data contained within the frame.
+    """
+    print data
+
 ser = serial.Serial(SERIALPORT, 9600)
-
-xbee = XBee(ser)
+xbee = ZigBee(ser, callback=print_data)
 
 # Continuously read and print packets
 while True:
     try:
-        response = xbee.wait_read_frame()
-        print response
+        time.sleep(0.1)
     except KeyboardInterrupt:
         break
 
+xbee.halt()
 ser.close()
---
+++ mesh/simple.py
@@ -0,0 +1,24 @@
+import serial
+from xbee import ZigBee
+import time
+
+SERIALPORT = '/dev/tty.usbserial-A602N3AY'
+ser = serial.Serial(SERIALPORT, 9600)
+xbee = ZigBee(ser)
+
+# replace with your router's address
+myRouter = '\x00\x13\xa2\x00\x40\xbf\x05\x76'
+
+while True:
+    try:
+        xbee.remote_at(dest_addr_long=myRouter, command='D4', parameter='\x05')
+        time.sleep(1)
+
+        xbee.remote_at(dest_addr_long=myRouter, command='D4', parameter='\x04')
+        time.sleep(1)
+
+    except KeyboardInterrupt:  # Ctrl-c
+        break
+
+xbee.halt()
+ser.close()
