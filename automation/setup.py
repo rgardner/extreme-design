2474ee6257d5d9decdef0868d2174d0ce86fcd79
Robert Gardner <rhg259@nyu.edu>
2014-11-15 14:48:02 -0500

    Add setup file

---
+++ automation/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='automation',
+      version='0.1',
+      description='Energy Sensing and Automation',
+      url='https://github.com/rgardner/extreme-design',
+      author='Robert Gardner',
+      author_email='robert.gardner@nyu.edu',
+      license='MIT',
+      packages=['automation'],
+      zip_safe=False)
