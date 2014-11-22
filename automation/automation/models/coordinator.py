68005d32cfbcc8017375153379c2c215214381ca
Robert Gardner <rhg259@nyu.edu>
2014-11-22 14:16:19 -0500

    Fix node discover

--- automation/automation/models/coordinator.py
+++ automation/automation/models/coordinator.py
@@ -1,9 +1,6 @@
-import logging
-import time
 import xbee
 
 from .node import Node
-from .packet import Packet
 
 
 class Coordinator(xbee.ZigBee):
@@ -11,7 +8,18 @@ class Coordinator(xbee.ZigBee):
 
     NAME = 'ms'
 
-    def actuate(self, node):
+    def __init__(self, serial_port):
+        super(Coordinator, self).__init__(serial_port)
+        self.name = self.NAME
+
+    def discover_nodes(self):
+        """Send node discover command."""
+        # Set node discover time to FF; send node discover from self.
+        self.send('at', frame='A', command='NT', parameter='\xFF')
+        self.at(command='ND')
+
+    def toggle_node_relay(self, node):
+        """Toggle the relay attached to the given node."""
         if node.relay_circuit_closed:
             self.remote_at(dest_addr_long=node.source_addr_long,
                            command=node.actuate_pin,
@@ -20,38 +28,3 @@ class Coordinator(xbee.ZigBee):
             self.remote_at(dest_addr_long=node.source_addr_long,
                            command=node.actuate_pin,
                            parameter=Node.RELAY_CLOSED_PARAM)
-
-    def discover_nodes(self, discover_time):
-        """Discover nodes on network for `time` secs."""
-        # Set node discover time to FF; send node discover from self.
-        self.send('at', frame='A', command='NT', parameter='\xFF')
-        self.at(command='ND')
-
-        nodes = {}  # source_addr_long: node object, includes self.
-        future = time.time() + discover_time
-        while time.time() < future:
-            response = self.wait_read_frame()
-            if ('command' not in response) or (response['command'] != 'ND'):
-                # Not a node discovery response packet.
-                logging.debug("non-nd response packet found: %s", response)
-                continue
-
-            logging.debug("nd response packet found: %s", response)
-            source_addr_long = response['parameter']['source_addr_long']
-            if source_addr_long not in nodes:
-                name = response['parameter']['node_identifier']
-                if name != self.NAME:
-                    nodes[source_addr_long] = Node(source_addr_long, name)
-
-        logging.debug("Coordinator discovered nodes: %s", nodes)
-        return nodes
-
-    def receive_sensor_data(self, receive_time):
-        """Recieve sensor data."""
-        responses = []
-        future = time.time() + receive_time
-        while time.time() < future:
-            packet = Packet(self.wait_read_frame())
-            logging.debug(packet)
-            responses.append(packet)
-        return responses
--- automation/automation/server.py
+++ automation/automation/server.py
@@ -19,8 +19,18 @@ class Server(object):
         for worker in self.workers:
             worker.start()
 
+        self.logger.debug("Discovering nodes on network.")
+        self.coordinator.discover_nodes()
+
         self.logger.debug("Starting the producer...")
         self.producer.start()
+
+    def shutdown(self):
+        self.logger.info("Shutting down server...")
+        self.producer.shutdown()
+        self.logger.debug("Blocking until all consumers are done...")
+        self.frame_queue.join()
+        self.logger.info("All done :)")
 
     def _setup(self):
         # Setup node manager.
@@ -33,16 +43,9 @@ class Server(object):
         self.producer = Producer(self.nodes, self.frame_queue)
 
         # Create consumers.
-        self.logger.debug("Spawning consumer processes...")
+        self.logger.debug("Spawning consumers...")
         for i in range(self.num_workers):
             p = Consumer(args=(self.frame_queue, self.nodes))
             p.setName("Consumer {0}".format(i))
             p.daemon = True
             self.workers.append(p)
-
-    def shutdown(self):
-        self.logger.info("Shutting down server...")
-        self.producer.shutdown()
-        self.logger.debug("Blocking until all consumers are done...")
-        self.frame_queue.join()
-        self.logger.info("All done :)")
